de7f7fa0be702485c23ff5e65a7f6a70d51a876b
Jean-Alexandre Peyroux <m@j4.pe>
2014-08-24 00:29:27 +0200

    Prise en compte de Photon (BANO). Code à vérifier par Eric

--- proxy/GeocodePhotonProxy.php
+++ proxy/GeocodePhotonProxy.php
@@ -1,10 +1,10 @@
 <?php
 
-$url_photon = 'http://france.photon.fluv.io/api/';
-
+//$url_photon = 'http://france.photon.fluv.io/api/';
 $sParamAddr = isset($_REQUEST['addr']) ? $_REQUEST['addr'] : "";
 $sParamCpVille = isset($_REQUEST['cpVille']) ? $_REQUEST['cpVille'] : "";
-$sParamUrlGeocoder = isset($_REQUEST['urlGeocoder']) ? $_REQUEST['urlGeocoder'] : "";
+$sParamProxy = isset($_REQUEST['proxy']) ? $_REQUEST['proxy'] : false;
+$sParamUrlGeocoder = isset($_REQUEST['urlGeocoder']) ? $_REQUEST['urlGeocoder'] : False;
 
 try {
 
@@ -16,11 +16,10 @@ try {
     $uri = $sParamUrlGeocoder  . '?' . http_build_query($aGeocodeParams);
     
     if($sParamCpVille == "") throw new Exception("Paramètre [cpVille] absent");
-    
+
     $jsonResponse = file_get_contents($uri);
     
     echo $jsonResponse;
-    
 
 }catch(Exception $ex) {
     echo json_encode(
