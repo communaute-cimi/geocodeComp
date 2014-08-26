c5a961627f74e37a0c8f72c80f59fd08600119da
Brice Person <brice@ideeslibres.org>
2014-08-26 13:27:11 +0200

    Modifications pour inclure IdeesLibres (BANO)

---
+++ proxy/GeocodeIdeesLibresProxy.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('../config.inc.php');
+
+$sParamAddr = isset($_REQUEST['addr']) ? $_REQUEST['addr'] : "";
+$sParamCpVille = isset($_REQUEST['cpVille']) ? $_REQUEST['cpVille'] : "";
+$sParamProxy = isset($_REQUEST['proxy']) ? $_REQUEST['proxy'] : FALSE;
+$sParamUrlGeocoder = isset($_REQUEST['urlGeocoder']) ? $_REQUEST['urlGeocoder'] : FALSE;
+
+try {
+        
+    if($sParamCpVille == "") throw new Exception("Paramètre [cpVille] absent");        
+
+    $aGeocodeParams = array(
+        'q' => $sParamAddr . " " . $sParamCpVille,
+        'limit' => 1
+    );
+    
+    $uri = $sParamUrlGeocoder  . '?' . http_build_query($aGeocodeParams);
+
+    if($sParamProxy == true) {
+        $jsonResponse = RequestProxy::getCurl($uri, $oJsonConf['proxy']['host'], $oJsonConf['proxy']['port'], $oJsonConf['proxy']['user'], $oJsonConf['proxy']['pwd']);
+    }else {
+        $jsonResponse = file_get_contents($uri);
+    }
+   
+    echo $jsonResponse;
+
+}catch(Exception $ex) {
+    echo json_encode(
+        array(
+            'error' => 1, 
+            'message' => $ex->getMessage()
+        )
+    );
+}
